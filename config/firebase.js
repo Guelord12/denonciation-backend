8f41ea73553caefb92abad485daf039be38ebe6d
Guelord12 <kayayaguelord8@gmail.com>
2026-02-27 20:21:29 +0100

    Fix firebase config path

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 // backend/config/firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./denonciation-ac6d1-firebase-adminsdk.json');
+const serviceAccount = require('../denonciation-ac6d1-firebase-adminsdk.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
